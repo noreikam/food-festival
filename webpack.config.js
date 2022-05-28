473218fae9c9542af2f018b64d527872fb8a6de3
noreikam <noreikam1@gmail.com>
2022-05-28 16:11:17 -0400

    manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -38,14 +39,29 @@ module.exports = {
     ]
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-  new BundleAnalyzerPlugin({
-    analyzerMode: "static"
-  })
-  ],
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static"
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies", 
+      description: "An app that allows you to view upcoming food events",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve('assets/img/icons/icon-512x512.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
+    })
+  ],  
   devServer: {
     static: {
       directory: path.join(__dirname, './'),
